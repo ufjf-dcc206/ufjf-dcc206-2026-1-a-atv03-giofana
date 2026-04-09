42c1da1acab9b4024c875d2604dee83876d24070
giovana <giovana.maieli@codejr.com.br>
2026-04-08 22:18:19 -0300

    :sparkles: adiciona root

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import './style.css'
-import { setupCounter } from './counter.js'
 import { setupRootApp } from './setupRoutApp.js';
 
-document.querySelector('#app').innerHTML = setupRootApp();
-
-setupCounter(document.querySelector('#counter'))
+document.querySelector('#app').append(setupRootApp());
