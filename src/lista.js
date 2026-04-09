9a1069fa572e6c1cc97b49c852b6f387da0b5c89
giovana <giovana.maieli@codejr.com.br>
2026-04-08 22:09:50 -0300

    :sparkles:adiciona limpa

--- src/lista.js
+++ src/lista.js
@@ -1,13 +1,21 @@
-const lista = ["um", "dois", "três"]; 
+let lista = [];
+
+export function iniciaLista(){
+    lista = ["um", "dois", "três"];
+}
 
 export function getLista() {
-    const novaLista = []; // Cria uma nova lista com os mesmos elementos
+    //const novaLista = []; // Cria uma nova lista com os mesmos elementos
 
     /*for(let i = 0; i < lista.length; i++) {
         novaLista.push(lista[i]);
     } */
    
-    return novaLista;
+    return structuredClone(lista);
     //return [...lista]; // Outra forma de criar uma nova lista com os mesmos elementos 
     // não precisa colocar o for dessa forma
+}
+
+export function limpaLista(){
+    lista.splace(0);
 }
--- src/lista.test.js
+++ src/lista.test.js
@@ -1,16 +1,27 @@
 import { expect, it } from "vitest";
-import { getLista } from "./lista";
+import { getLista, iniciaLista, limpaLista } from "./lista";
 
 it("deve ter elementos iniciais", () => {
+    iniciaLista();
     const lista = getLista();
     expect(lista.length).toBe(3);
 });
 
 it("alterar a lista retornada, não pode mexer na do módulo", () => {
+    iniciaLista();
     const lista1 = getLista();
     expect(lista1.length).toBe(3);
     lista1.splice(0, 1);
 
     const lista2 = getLista();
     expect(lista2.length).toBe(3);
+});
+
+it("deve ter zero elementos após limpar a lista", ()=>{
+    iniciaLista();
+    const lista1 = getLista();
+    expect(lista1.length).toBe(3);
+    limpaLista();
+    const lista2 = getLista();
+    expect(lista2.length).toBe(0);
 });
